4a20d3397ebe5ce35ec1b43d3f73e0216d1b6636
Unam <97320894+manukao@users.noreply.github.com>
2023-03-23 12:36:32 +0100

    create server

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 import { createServer } from "node:http";
 
-// export const server = …
+export const server = createServer((request, response) => {
+  if (request.url === "/") {
+    response.statusCode = 200;
+    response.end("Hello, [Your name]!");
+  } else {
+    response.statusCode = 404;
+    response.end("Not Found");
+  }
+});
